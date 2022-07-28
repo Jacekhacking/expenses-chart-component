83d86c3ea5cfb60f95f95fa33cd9ecb957512632
JacekHacking <Jacekhacking@mac.com>
2022-07-28 12:22:02 -0600

    grid implemented flex layout finished

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Footer from "./components/footer";
 import styled from "styled-components";
 import Logo from "./images/logo.svg";
+import Graph from "./components/Graph";
+import "charts.css";
 
 const ComponentWrapper = styled.div`
   padding: 40px;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
   min-height: 500px;
   flex-direction: column;
@@ -15,18 +17,33 @@ const ComponentWrapper = styled.div`
 const RedContainer = styled.div`
   background-color: hsl(10, 79%, 65%);
   color: white;
-  padding: 30px;
+  padding: 20px;
   border-radius: 10px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 360px;
 `;
 
+const WhiteContainer = styled.div`
+  background-color: white;
+  min-height: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  border-radius: 10px;
+  margin-top: 5px;
+  width: 400px;
+`;
+const FlexContainer = styled.div`
+  display: flex;
+`;
 function App() {
   return (
     <>
       <ComponentWrapper>
-        <div>
+        <>
           <RedContainer>
             <div>
               <p>My balance</p>
@@ -34,12 +51,41 @@ function App() {
             </div>{" "}
             <img src={Logo} alt="logo" />
           </RedContainer>
-          <div>
-            <h2>Spending - Last 7 days</h2>
-            mon tue wed thu fri sat sun
-            <div>Total this month $478.33 +2.4% from last month</div>
-          </div>
-        </div>
+
+          <WhiteContainer>
+            <Graph />
+
+            <FlexContainer
+              style={{
+                width: "360px",
+                margin: " 5px 20px",
+                paddingTop: "30px",
+                borderTop: "2px solid black",
+                justifyContent: "space-between",
+                color: "hsl(28, 10%, 53%)",
+              }}
+            >
+              <div>
+                Total this month
+                <section style={{ color: "hsl(25, 47%, 15%)" }}>
+                  $478.33
+                </section>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  flexStart: "flex-end",
+                  alignItems: "flex-end",
+                }}
+              >
+                <section style={{ color: "hsl(25, 47%, 15%)" }}>+2.4%</section>
+                from last month
+              </div>
+            </FlexContainer>
+          </WhiteContainer>
+        </>
       </ComponentWrapper>
       <Footer></Footer>
     </>
---
+++ src/components/Graph.js
@@ -0,0 +1,44 @@
+const Graph = () => {
+  return (
+    <>
+      <table
+        class="charts-css column show-heading show-labels show-primary-axis data-spacing-10"
+        id="my-chart"
+      >
+        <caption>Spending - Last 7 days</caption>
+        <tbody>
+          <tr>
+            <th scope="row">mon</th>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">tue</th>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">wed</th>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">thu</th>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">fri</th>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">sat</th>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">sun</th>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Graph;
